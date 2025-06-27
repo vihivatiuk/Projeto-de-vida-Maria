5990df5e7de32cffa3be6fade9d409bfc31eefb2
Maria Vitoria <132943819+vihivatiuk@users.noreply.github.com>
2025-06-27 09:09:31 -0500

    Add files via upload

--- projeto-de-vida-3a-main/js-projeto-de-vida-codigo-base/main.js
+++ projeto-de-vida-3a-main/js-projeto-de-vida-codigo-base/main.js
@@ -17,17 +17,32 @@ for(let i=0;i < botoes.length; i++){
 const contadores = document.querySelectorAll (".contador");
 
 const tempoObjetivo1 = new Date("2030-12-05T00:00:00");
-contadores[0] .textContent = calculaTempo (tempoObjetivo1);
-const tempoObjetivo2 = new Date("2031-12-10T00:00:00");
-contadores[1] .textContent = calculaTempo (tempoObjetivo2);
-const tempoObjetivo3 = new Date("2033-10-06T00:00:00");
-contadores[2] .textContent = calculaTempo (tempoObjetivo3);
-const tempoObjetivo4 = new Date("2034-08-10T00:00:00");
-contadores[3] .textContent = calculaTempo (tempoObjetivo4);
+contadores[0].textContent = calculaTempo (tempoObjetivo1);
 
-function calculaTempo(tempoObjetivo){
-let tempoAtual = new Date();
-let tempoFinal = tempoObjetivo - tempoAtual;
-let segundos = Math.floor (tempoFinal/1000);
-return segundos; 
+const tempoObjetivo2 = new Date("2031-12-10T00:00:00");
+contadores[1].textContent = calculaTempo (tempoObjetivo2);
+
+const tempoObjetivo3 = new Date("2032-10-06T00:00:00");
+contadores[2].textContent = calculaTempo (tempoObjetivo3);
+
+const tempoObjetivo4 = new Date("2034-08-10T00:00:00");
+contadores[3].textContent = calculaTempo (tempoObjetivo4);
+
+function calculaTempo (tempoObjetivo){
+    let tempoAtual = new Date ();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor (tempoFinal/1000);
+    let minutos = Math.floor (segundos/60);
+    let horas = Math.floor (minutos/60);
+    let dias = Math.floor (horas/24);
+    let anos = Math.floor (dias/365);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+    dias %= 365;
+
+
+    return anos + " anos " + dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
+
 }
